a82707fb0d6c338c7eedd732d14804016053d601
Fang Hau <cozyazure@hotmail.com>
2017-02-18 09:46:51 +0800

    done with word counting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # php-rest-api
 Simple PHP rest api
+
+Write a small web service in PHP that accepts a POST request, with the body containing a bunch of text. The service should count the top ten most used words in the document and report them along with how often they occur.
--- www/index.php
+++ www/index.php
@@ -1,1 +1,17 @@
-<? echo "<p>Hello?</p>"; ?>
+<?php
+    header('Content-type:application/json;charset=utf-8');
+    $method = $_SERVER['REQUEST_METHOD'];
+    //ensure its a post
+    if($method == 'POST'){
+        $data = json_decode(file_get_contents('php://input'), true);
+        $str = $data['input'];
+        //truncate and count the wrords
+        $words = array_count_values(str_word_count($str, 1)); 
+        //sort the words according to count;
+        arsort($words);
+        //get the first 10 highest occurence words
+        $output = array_slice($words, 0, 10);
+        echo json_encode($output);
+    }
+
+?>
